51d24d348d89a36b46af33fb05b9941b8c542601
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2022-09-04 12:21:25 -0300

    Solve Two Sum problem

---
+++ arrays/two_sum.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+// O(N) Time complexity | O(N) Space complexity
+vector<int> twoNumberSum(vector<int> array, int targetSum) {
+  
+  set<int> seen;
+  
+  for (int i = 0; i < array.size(); ++i) {
+    int potential_match = targetSum - array[i];
+    if (seen.count(potential_match) > 0)
+      return {array[i], potential_match};
+    seen.insert(array[i]);
+  }
+
+  return {};
+}
